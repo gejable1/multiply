05aeb8b1d96ef9a7f280f2e6aff32cc4a31bd8ab
gejable1 <72341183+gejable1@users.noreply.github.com>
2026-08-28 10:50:54 +0800

    Add files via upload

--- multiply-sw.js
+++ multiply-sw.js
@@ -25,7 +25,7 @@
 //       .then(() => self.registration.unregister())); });
 // ─────────────────────────────────────────────────────────────────────────
 
-const CACHE_VERSION = 'multiply-shell-v10-2026-08-17';
+const CACHE_VERSION = 'multiply-shell-v11-2026-08-28';
 const SHELL_ASSETS = [
   './',
   'index.html',
